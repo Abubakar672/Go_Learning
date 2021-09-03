177c4ad936cd5b49a3787eba218337d4701ae9fd
Abubakar Mujahid <abubakarmujahid1@live.com>
2021-09-03 06:08:23 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,114 @@
+/*
+
+GO lEARNING
+Authors: Ali Wisam, AbuBakar Mujahid
+
+*/
+
+
+/*whenever we want to start the code of go we have to import the main package like package main    
+import fmt used for formatting the code in the golang
+and your codes end on the main function
+
+command: go fmt
+
+if in terminal you will go to the folder and enter the command of the go fmt so it will format all the code according to the go lang 
+
+& 
+command: go fmt ./...
+
+if you will enter the go fmt ./... this command in a repo so it will format all the code files in that repo into the golang format
+*/
+
+//Simple program to print Hello Wordl in Go
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//   fmt.Println("Hello World")
+// }
+
+
+/*
+command: go run <filename>
+As for the case if you want to run your .go file through terminal and want to see output so simply in terminal go to the specific folder and enter the command go run <filename>
+
+command: go build
+
+if you enter the command go build in that specific folder it will build the exe file of the program
+
+command: ./..go build
+
+*/
+
+
+/*
+
+Control Flows in Go lang
+
+There are three type of control flow in Go 
+Control flow is the flow control in which the function call are been executed means it is the flow that how the computer will read the code 
+
+1)Sequential 
+2)Loop Iterative
+3)Conditional 
+
+*/
+
+
+//First Program in Go to print data 
+
+// package main 
+
+// import "fmt" 
+
+// func main(){
+
+//   fmt.Println("Hello ABubkar")
+// }
+
+
+//Go Programming codes here 
+
+
+/*
+**************************************************************
+*                         CODE 1                             *
+**************************************************************
+*/
+
+
+// package main
+
+// import "fmt"
+
+// func main(){
+//   fmt.Println("a")
+//   foo()
+//   even()
+// }
+
+
+// func foo(){
+//   fmt.Println("HI ")
+// }
+
+// func even(){
+// for i:=0 ;i<100;i++{
+//   if i%2==0{
+//   fmt.Println(i)
+// }}
+
+// }
+
+
+
+/*
+**************************************************************
+*                         CODE 2                             *
+**************************************************************
+*/
+
+
